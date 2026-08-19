5ad2ed4fb773a5cb749917dff5a2dab620750132
yohanpache <143036644+yohanpache@users.noreply.github.com>
2026-08-19 11:40:47 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router base={import.meta.env.BASE_URL}>
+    <App />
+  </Router>
+);
